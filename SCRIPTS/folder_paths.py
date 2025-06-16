9133898784107acfb45cea7591a9ed9f5268e2ea
Fabio Brugnara <fabio.brugnara@hotmail.it>
2025-06-16 15:49:26 +0200

    a

---
+++ SCRIPTS/folder_paths.py
@@ -0,0 +1,10 @@
+RAW_folder = '../RAW_DATA/'
+MASKS_folder = '../MASKS/'
+PROCESSED_folder = '../PROCESSED_DATA/'
+
+Intensity_folder = '../PROCESSED_DATA/Intensity/'
+g24damaged_vGeO2_folder = '../PROCESSED_DATA/g24damaged_vGeO2/'
+g2fit4damaged_vGeO2_folder = '../PROCESSED_DATA/g2fit4damaged_vGeO2/'
+
+pilatus_LaB6_poni = '../PROCESSED_DATA/LaB6_calib/LaB6_calib_scan4.poni'
+
